1fec395533cd1250342c09faa15dc31d9a6f757a
Michael Smith <mijamessmith@gmail.com>
2020-10-27 10:56:51 -0700

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default function About() {
 
-    let about = "I got started in software with the classic high school Java class that left me thinking I'd never code again.\nFrom there I followed my dreams, became a professional pianist, earning a doctorate and a Carnegie Hall Juilliard fellowship on the way. \n I started getting back into code when I started learning Max / MSP in graduate school, a multimedia visual programing interface that allowed me to build audio processors for my own live playing.I couldn't shake the itch and started learning fullstack technologies on the side. Thanks for stopping by.Shoot me an email with any questions you may have."
+    let about = `I got started in software with the classic high school Java class that left me thinking I'd never code again.
+\nFrom there I followed my dreams, became a professional pianist, earning a doctorate and a Carnegie Hall Juilliard fellowship on the way.
+\n I started getting back into code while working with Max/MSP in graduate school, a multimedia visual programing interface that allowed me
+to build audio processors for my own live playing.I couldn't shake the itch and started learning fullstack technologies on the side.\n
+Thanks for stopping by. Shoot me an email with any questions you may have.`
 
     return (
         <div className="About" id="About">
